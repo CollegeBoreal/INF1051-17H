95e3d9b00f21428b9281f54406ea3d7d4c2c2918
to-boreal <to-boreal@collegeboreal.ca>
2017-02-15 19:06:04 -0500

    example

---
+++ 4.ANGULAR/B300098957/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { ArticleComponent } from './article.component';
+import { NavigationComponent } from './navigation.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ArticleComponent,
+    NavigationComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
