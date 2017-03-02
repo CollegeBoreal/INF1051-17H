619450011e9b4f345573d597149c40fcd1715270
Julien L <Julien L>
2017-03-02 16:13:26 -0500

    fix pokemon

--- C.OKELIST/pokelist/src/app/app.routing.ts
+++ C.OKELIST/pokelist/src/app/app.routing.ts
@@ -1,12 +1,27 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PokemonComponent } from './pokemon/pokemon.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 const appRoutes: Routes = [
+  // {
+  //   path: '',
+  //   redirectTo: 'pokemon',
+  //    pathMatch: 'full'
+  // },
   {
-    path: '',
+    path: 'pokemon',
     component: PokemonComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
   }
 
 ];
--- C.OKELIST/pokelist/src/app/pokemon/pokemon.service.ts
+++ C.OKELIST/pokelist/src/app/pokemon/pokemon.service.ts
@@ -12,6 +12,7 @@ export class PokemonService {
     }
   
   pokemonInit():Array<any>{
+    console.log("pokemonInit");
         let pokemons: Array<any> = [];
         this.http.get("https://pokeapi.co/api/v2/pokemon?limit=151")
         .map( result => result.json())
